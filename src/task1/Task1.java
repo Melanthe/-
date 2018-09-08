3472f53dff45287abd77cd51265c8c86df018b5f
Yana Bernachkaya <yana.bernachkaya@gmail.com>
2018-09-08 23:45:37 +0300

    Task1 1: Split with function

--- src/task1/Task1.java
+++ src/task1/Task1.java
@@ -1,9 +1,25 @@
 package task1;
 
+import common.MyExceptions;
+
 public class Task1 {
     public static void main(String[] argh){
 
-        Taylor tl = new Taylor();
-        tl.sum();
+        try {
+            Taylor tl = new Taylor();
+            tl.init();
+            tl.sum();
+            tl.standartSum();
+            tl.show();
+        }
+        catch(MyExceptions e){
+            System.out.println(e);
+        }
+        catch(NumberFormatException e){
+            System.out.println("Invalid input!");
+        }
+        catch(Exception e){
+            System.out.println("Error!");
+        }
     }
 }
--- src/task1/Taylor.java
+++ src/task1/Taylor.java
@@ -3,48 +3,50 @@ package task1;
 import common.MyExceptions;
 
 import java.util.Scanner;
-import common.MyExceptions;
 
 public class Taylor {
 
-    public void sum(){
+    private int k;
+    private double x;
+    private  double eps;
+    private  double sum;
+    private  double stSum;
 
-        int k;
-        double x, eps, stSum,  sum = 1, prev = 1;
+    public void init() throws MyExceptions {
 
         Scanner sc = new Scanner(System.in);
 
-        try {
+        System.out.println("Input k and x:");
 
-            System.out.println("Input k and x:");
-            k = Integer.parseInt(sc.next());
-            x = Double.parseDouble(sc.next());
+        k = Integer.parseInt(sc.next());
+        x = Double.parseDouble(sc.next());
 
-            if (k <= 0) {
-                throw new MyExceptions("Accuracy must be greater than 0!");
-            }
-
-            eps = Math.pow(10, -k);
-
-            for(int i = 1; Math.abs(prev) >= eps; ++i){
-                prev *= x / i;
-                sum += prev;
-            }
-
-            stSum = Math.exp(x);
-
-            System.out.printf("Result: %.3f \n", sum);
-            System.out.printf("The result of the standard function: %.3f", stSum);
-
+        if (k <= 0) {
+            throw new MyExceptions("Accuracy must be greater than 0!");
         }
-        catch(MyExceptions e){
-            System.out.println(e);
+
+        eps = Math.pow(10, -k);
+        sum = 1;
+    }
+
+    public void sum() {
+
+        double prev = 1;
+
+        for (int i = 1; Math.abs(prev) >= eps; ++i) {
+            prev *= x / i;
+            sum += prev;
         }
-        catch(NumberFormatException e){
-            System.out.println("Invalid input!");
-        }
-        catch(Exception e){
-            System.out.println("Error!");
-            }
+    }
+
+    public void standartSum() {
+
+        stSum = Math.exp(x);
+    }
+
+    public void show() {
+
+        System.out.printf("Result: %.3f \n", sum);
+        System.out.printf("The result of the standard function: %.3f", stSum);
     }
 }
